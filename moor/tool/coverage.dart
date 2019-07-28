053d4280fc0be27d0b961b0a7d5ad04460475e62
Simon Binder <oss@simonbinder.eu>
2019-07-28 10:38:18 +0200

    Only format coverage once

--- moor/tool/coverage.dart
+++ moor/tool/coverage.dart
@@ -6,8 +6,6 @@ import 'package:grinder/grinder_sdk.dart';
 import 'package:coverage/coverage.dart';
 import 'package:path/path.dart';
 
-import 'format_coverage.dart' as fc;
-
 Future<void> main(List<String> args) async {
   // First, generate the build script, see
   // https://github.com/dart-lang/build/blob/3208cfe94c475ed3e1ec44c227aadaddaeac263d/build_runner/bin/build_runner.dart#L65
@@ -19,7 +17,4 @@ Future<void> main(List<String> args) async {
   final coverage = await runAndCollect(tests, onExit: true, printOutput: true);
 
   File('coverage.json').writeAsStringSync(json.encode(coverage));
-
-  print('formatting to .lcov format');
-  await fc.main();
 }
