3e286e27afe7fc8ed7aa7c7a5f086a9d9d0bc7b5
Simon Binder <oss@simonbinder.eu>
2019-09-24 21:53:14 +0200

    Improve performance of blob reading/writing with ffi

--- moor_ffi/lib/src/ffi/blob.dart
+++ moor_ffi/lib/src/ffi/blob.dart
@@ -11,11 +11,12 @@ class CBlob extends Struct<CBlob> {
   int data;
 
   static Pointer<CBlob> allocate(Uint8List blob) {
-    final str = Pointer<CBlob>.allocate(count: blob.length);
-    for (var i = 0; i < blob.length; i++) {
-      str.elementAt(i).load<CBlob>().data = blob[i];
-    }
-    return str;
+    final str = Pointer<Uint8>.allocate(count: blob.length);
+
+    final asList = str.asExternalTypedData(count: blob.length) as Uint8List;
+    asList.setAll(0, blob);
+
+    return str.cast();
   }
 
   /// Allocates a 0-terminated string, encoded as utf8 and read from the
@@ -30,18 +31,25 @@ class CBlob extends Struct<CBlob> {
   /// Reads [bytesToRead] bytes from the current position.
   Uint8List read(int bytesToRead) {
     assert(bytesToRead >= 0);
-    final str = addressOf;
+    final str = addressOf.cast<Uint8>();
     if (isNullPointer(str)) return null;
 
-    // todo can we user Pointer.asExternalTypedData here?
-    final blob = Uint8List(bytesToRead);
-    for (var i = 0; i < bytesToRead; ++i) {
-      blob[i] = str.elementAt(i).load<CBlob>().data;
-    }
-    return blob;
+    final data = str.asExternalTypedData(count: bytesToRead) as Uint8List;
+    return Uint8List.fromList(data);
+  }
+
+  /// More efficient version of [readString] that doesn't have to find a nil-
+  /// terminator. [length] is the amount of bytes to read. The string will be
+  /// decoded via [utf8].
+  String readAsStringWithLength(int length) {
+    return utf8.decode(read(length));
   }
 
   /// Reads a 0-terminated string, decoded with utf8.
+  ///
+  /// Warning: This method is very, very slow. If there is any way to know the
+  /// length of the string to read, [readAsStringWithLength] will be orders of
+  /// magnitude faster.
   String readString() {
     final str = addressOf;
     if (isNullPointer(str)) return null;
