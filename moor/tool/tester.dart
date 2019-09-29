5a0c0d9212d46752930349631606e1dfd644b1a4
Simon Binder <oss@simonbinder.eu>
2019-09-29 11:27:35 +0200

    Use package:test_coverage for moor subproject

--- moor/tool/tester.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test_core/src/executable.dart' as test;
-
-void main() async {
-  print('inside test file');
-  // Run tests
-  await test.main([]);
-  print('done, script should terminate after coverage is collected');
-}
