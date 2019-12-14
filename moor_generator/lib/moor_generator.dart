c69b7cb2839294e1d6a6d6971d6a15bde12b6484
Simon Binder <oss@simonbinder.eu>
2019-12-14 21:49:51 +0100

    Move builders and analyzer plugin into subfolder

--- moor_generator/lib/moor_generator.dart
+++ moor_generator/lib/moor_generator.dart
@@ -1,6 +1,1 @@
-import 'package:build/build.dart';
-import 'package:moor_generator/src/backends/build/moor_builder.dart';
-
 export 'src/model/model.dart';
-
-Builder moorBuilder(BuilderOptions options) => MoorBuilder(options);
