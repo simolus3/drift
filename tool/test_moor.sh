727ab4d88a402cb0bd4022b6d80090577ccd408b
Simon Binder <oss@simonbinder.eu>
2020-01-28 20:20:43 +0100

    CI: Disable testing on the web

--- tool/test_moor.sh
+++ tool/test_moor.sh
@@ -7,8 +7,9 @@ pub run build_runner build --delete-conflicting-outputs
 
 pub run test --coverage=coverage || EXIT_CODE=$?
 
-if [ $EXIT_CODE -eq 0 ]
-  then pub run test -P browsers || EXIT_CODE=$?
-fi
+# Testing on the web doesn't work on CI (we're running into out-of-memory issues even with 12G of RAM)
+#if [ $EXIT_CODE -eq 0 ]
+#  then pub run test -P browsers || EXIT_CODE=$?
+#fi
 
 exit $EXIT_CODE
