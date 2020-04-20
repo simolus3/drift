1da09e73571e562ee8f5dae5efb4ac5dc5241ac0
Simon Binder <oss@simonbinder.eu>
2020-04-20 22:03:45 +0200

    Add small integration test for generated upserts

---
+++ moor/test/integration_tests/insert_integration_test.dart
@@ -0,0 +1,27 @@
+import 'package:moor/moor.dart';
+import 'package:moor_ffi/moor_ffi.dart';
+@TestOn('vm')
+import 'package:test/test.dart';
+
+import '../data/tables/todos.dart';
+
+void main() {
+  TodoDb db;
+
+  setUp(() {
+    db = TodoDb(VmDatabase.memory());
+  });
+
+  test('insertOnConflictUpdate', () async {
+    await db.into(db.categories).insert(
+        CategoriesCompanion.insert(description: 'original description'));
+
+    var row = await db.select(db.categories).getSingle();
+
+    await db.into(db.categories).insertOnConflictUpdate(CategoriesCompanion(
+        id: Value(row.id), description: const Value('changed description')));
+
+    row = await db.select(db.categories).getSingle();
+    expect(row.description, 'changed description');
+  });
+}
