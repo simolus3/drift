e6a51b5ac4e3572208870c8f944587854e673ac2
Simon Binder <oss@simonbinder.eu>
2020-06-24 17:47:14 +0200

    moor_ffi: Build with latest sqlite3 and rtree

--- extras/integration_tests/flutter_db/lib/moor_ffi.dart
+++ extras/integration_tests/flutter_db/lib/moor_ffi.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/widgets.dart' show WidgetsFlutterBinding;
+import 'package:moor_ffi/database.dart' as raw;
 import 'package:moor_ffi/moor_ffi.dart';
 import 'package:test/test.dart';
 import 'package:tests/tests.dart';
@@ -35,6 +36,40 @@ Future<void> main() async {
   final dbPath = await getDatabasesPath();
   Directory(dbPath).createSync(recursive: true);
   runAllTests(FfiExecutor(dbPath));
+
+  test('supports the rtree module', () {
+    final db = raw.Database.memory();
+
+    db.execute('''
+      CREATE VIRTUAL TABLE demo_index USING rtree(
+        id,              -- Integer primary key
+        minX, maxX,      -- Minimum and maximum X coordinate
+        minY, maxY       -- Minimum and maximum Y coordinate
+      );
+
+      INSERT INTO demo_index VALUES(
+        1,                   -- Primary key -- SQLite.org headquarters
+       -80.7749, -80.7747,  -- Longitude range
+       35.3776, 35.3778     -- Latitude range
+      );
+
+      INSERT INTO demo_index VALUES(
+        2,                   -- NC 12th Congressional District in 2010
+        -81.0, -79.6,
+        35.0, 36.2
+      );
+    ''');
+
+    final stmt = db.prepare('''
+      SELECT id FROM demo_index
+        WHERE minX>=-81.08 AND maxX<=-80.58
+        AND minY>=35.00  AND maxY<=35.44;
+    ''');
+
+    expect(stmt.select().single['id'], 1);
+
+    db.close();
+  });
 
   test('isolates integration test', () async {
     // This test exists to verify that our communication protocol works when we
