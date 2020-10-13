fc034701d05633b19cddd6eb2f836611f33de0e8
Simon Binder <oss@simonbinder.eu>
2020-10-13 18:19:06 +0200

    Use new Dart tool for formatting and analysis

--- tool/analyze.sh
+++ tool/analyze.sh
@@ -4,8 +4,8 @@ EXIT_CODE=0
 
 cd moor
 echo $(pwd)
-dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-dartanalyzer --options analysis_options.yaml --fatal-infos --fatal-warnings lib/ test/ || EXIT_CODE=$?
+dart format -o none --set-exit-if-changed . || EXIT_CODE=$?
+dart analyze --fatal-infos --fatal-warnings || EXIT_CODE=$?
 cd ..
 
 # todo we don't analyze this subproject because Flutter isn't installed in the CI
@@ -23,13 +23,13 @@ cd ..
 
 cd moor_generator
 echo $(pwd)
-dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-dartanalyzer --options analysis_options.yaml --fatal-infos --fatal-warnings lib/ test/ || EXIT_CODE=$?
+dart format -o none --set-exit-if-changed . || EXIT_CODE=$?
+dart analyze --fatal-infos --fatal-warnings || EXIT_CODE=$?
 cd ..
 
 cd sqlparser
 echo $(pwd)
-dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-dartanalyzer --options analysis_options.yaml --fatal-infos --fatal-warnings lib/ test/ || EXIT_CODE=$?
+dart format -o none --set-exit-if-changed . || EXIT_CODE=$?
+dart analyze --fatal-infos --fatal-warnings || EXIT_CODE=$?
 
 exit $EXIT_CODE
