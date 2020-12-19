fa0cda6ffe2356011d527232be23443d810fedb0
Simon Binder <oss@simonbinder.eu>
2020-12-19 17:34:02 +0100

    Add regression test for #978

---
+++ sqlparser/test/parser/regression_978_test.dart
@@ -0,0 +1,34 @@
+import 'package:sqlparser/sqlparser.dart';
+import 'package:test/test.dart';
+
+import 'utils.dart';
+
+void main() {
+  test('parses CREATE TABLE statement', () {
+    testStatement(
+      'CREATE TABLE \"sample_table\" ('
+      'id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, sample VARCHAR NULL)',
+      CreateTableStatement(
+        tableName: 'sample_table',
+        columns: [
+          ColumnDefinition(
+            columnName: 'id',
+            typeName: 'INTEGER',
+            constraints: [
+              NotNull(null),
+              PrimaryKeyColumn(null, autoIncrement: true),
+            ],
+          ),
+          ColumnDefinition(
+            columnName: 'sample',
+            typeName: 'VARCHAR',
+            constraints: [
+              NullColumnConstraint(null),
+            ],
+          ),
+        ],
+      ),
+      moorMode: true,
+    );
+  });
+}
