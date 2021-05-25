6ffd4254aaf1c8a291d5c771b37ac5a78a108b27
Peter Leibiger <kuhnroyal@gmail.com>
2021-05-25 15:54:22 +0200

    Fix test case

--- moor/test/insert_test.dart
+++ moor/test/insert_test.dart
@@ -71,7 +71,19 @@ void main() {
   });
 
   test('notifies stream queries on insertReturning', () async {
-    await db.into(db.users).insertReturning(UsersCompanion(
+    when(executor.runSelect(any, any)).thenAnswer((_) {
+      return Future.value([
+        {
+          'id': 5,
+          'name': 'User McUserface',
+          'is_awesome': true,
+          'profile_picture': Uint8List(0),
+          'creation_time': DateTime.now().millisecondsSinceEpoch,
+        }
+      ]);
+    });
+
+    final user = await db.into(db.users).insertReturning(UsersCompanion(
           name: const Value('User McUserface'),
           isAwesome: const Value(true),
           profilePicture: Value(Uint8List(0)),
@@ -79,6 +91,8 @@ void main() {
 
     verify(streamQueries.handleTableUpdates(
         {const TableUpdate('users', kind: UpdateKind.insert)}));
+
+    expect(user.id, 5);
   }, skip: onNoReturningSupport());
 
   group('enforces integrity', () {
