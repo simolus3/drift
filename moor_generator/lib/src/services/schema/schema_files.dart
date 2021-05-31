c4de3607170857471dcc350ad1184b90dfcdeade
Simon Binder <oss@simonbinder.eu>
2021-05-31 21:02:11 +0200

    Omit client defaults from schema test (#1240)

--- moor_generator/lib/src/services/schema/schema_files.dart
+++ moor_generator/lib/src/services/schema/schema_files.dart
@@ -285,13 +285,14 @@ class SchemaReader {
         _columnFeature(feature)
     ];
 
+    // Note: Not including client default code because that usually depends on
+    // imports from the database.
     return MoorColumn(
       name: ColumnName.explicitly(name),
       dartGetterName: ReCase(name).camelCase,
       type: moorType,
       nullable: nullable,
       defaultArgument: data['default_dart'] as String,
-      clientDefaultCode: data['default_client_dart'] as String,
       customConstraints: customConstraints,
       features: dslFeatures,
     );
