dc60716a4af4f51141c8242aeca31cdae8188a04
Simon Binder <oss@simonbinder.eu>
2021-06-08 21:25:22 +0200

    Don't schedule work before a database is opened

--- moor/lib/src/utils/synchronized.dart
+++ moor/lib/src/utils/synchronized.dart
@@ -2,13 +2,13 @@ import 'dart:async';
 
 /// A single asynchronous lock implemented by future-chaining.
 class Lock {
-  Future<void> _last = Future.value();
+  Future<void>? _last;
 
   /// Waits for previous [synchronized]-calls on this [Lock] to complete, and
   /// then calls [block] before further [synchronized] calls are allowed.
   Future<T> synchronized<T>(FutureOr<T> Function() block) {
     final previous = _last;
-    // This controller may not be sync: It must complete just after
+    // This completer may not be sync: It must complete just after
     // callBlockAndComplete completes.
     final blockCompleted = Completer<void>();
     _last = blockCompleted.future;
@@ -21,6 +21,10 @@ class Lock {
       }
     }
 
-    return previous.then((_) => callBlockAndComplete());
+    if (previous != null) {
+      return previous.then((_) => callBlockAndComplete());
+    } else {
+      return callBlockAndComplete();
+    }
   }
 }
