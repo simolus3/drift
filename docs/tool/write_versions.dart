008c348aba3f721e4e13023e3430a86901623dfb
Simon Binder <oss@simonbinder.eu>
2021-08-20 18:33:53 +0200

    Automatically include latest version in docs

---
+++ docs/tool/write_versions.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:build/build.dart';
+import 'package:yaml/yaml.dart';
+
+Builder writeVersions(BuilderOptions options) {
+  return _VersionsBuilder();
+}
+
+class _VersionsBuilder extends Builder {
+  @override
+  Future<void> build(BuildStep buildStep) async {
+    const packages = [
+      'moor',
+      'moor_generator',
+      'sqlparser',
+      'path',
+      'build_runner',
+    ];
+
+    final versions = <String, String>{};
+    for (final package in packages) {
+      final pubspec =
+          await buildStep.readAsString(AssetId(package, 'pubspec.yaml'));
+      final parsedPubspec = loadYaml(pubspec);
+
+      versions[package] = (parsedPubspec as Map)['version'].toString();
+    }
+
+    await buildStep.writeAsString(
+        buildStep.allowedOutputs.single, json.encode(versions));
+  }
+
+  @override
+  Map<String, List<String>> get buildExtensions => const {
+        r'$package$': ['lib/versions.json']
+      };
+}
