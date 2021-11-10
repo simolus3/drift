cffbd428629691790935693d94757f2bf2043473
westito <nemeth@marcell.it>
2021-11-10 17:52:55 +0100

    Fix postgres integration test

--- extras/integration_tests/tests/lib/suite/crud_tests.dart
+++ extras/integration_tests/tests/lib/suite/crud_tests.dart
@@ -102,9 +102,15 @@ void crudTests(TestExecutor executor) {
     final db = Database(executor.createConnection());
 
     // ignore: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
-    await db.customStatement(
-        'INSERT INTO friendships (first_user, second_user) VALUES (?1, ?2)',
-        <int>[1, 2]);
+    if (db.executor.dialect == SqlDialect.postgres) {
+      await db.customStatement(
+          'INSERT INTO friendships (first_user, second_user) VALUES (@1, @2)',
+          <int>[1, 2]);
+    } else {
+      await db.customStatement(
+          'INSERT INTO friendships (first_user, second_user) VALUES (?1, ?2)',
+          <int>[1, 2]);
+    }
 
     expect(await db.friendsOf(1).get(), isNotEmpty);
     await executor.clearDatabaseAndClose(db);
