49668e3a578934f5e717bd93db5a0714634edec5
Simon Binder <oss@simonbinder.eu>
2022-04-12 18:30:44 +0200

    Preserve Uint8Lists in serialization (#1802)

--- drift/test/remote_test.dart
+++ drift/test/remote_test.dart
@@ -1,5 +1,7 @@
 import 'package:async/async.dart';
+import 'package:drift/drift.dart';
 import 'package:drift/remote.dart';
+import 'package:drift/src/remote/protocol.dart';
 import 'package:stream_channel/stream_channel.dart';
 import 'package:test/test.dart';
 
@@ -19,5 +21,28 @@ void main() {
     );
 
     await shutdown(transformed);
+  });
+
+  test('Uint8Lists are mapped from and to Uint8Lists', () async {
+    const protocol = DriftProtocol();
+
+    final request = Request(
+      1,
+      ExecuteQuery(StatementMethod.select, 'SELECT ?', [
+        Uint8List.fromList([1, 2, 3])
+      ]),
+    );
+
+    final mapped = protocol.deserialize(protocol.serialize(request)!);
+    expect(
+      mapped,
+      isA<Request>().having((e) => e.id, 'id', 1).having(
+            (e) => e.payload,
+            'payload',
+            isA<ExecuteQuery>()
+                .having((e) => e.method, 'method', StatementMethod.select)
+                .having((e) => e.args, 'args', [isA<Uint8List>()]),
+          ),
+    );
   });
 }
