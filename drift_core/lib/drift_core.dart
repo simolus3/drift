832f5475db6ae2d60eb331694fdc5eb4261325cf
Elliot Hesp <elliot.hesp@gmail.com>
2022-05-03 15:57:46 +0100

    Update drift_core.dart

--- drift_core/lib/drift_core.dart
+++ drift_core/lib/drift_core.dart
@@ -6,6 +6,7 @@ export 'src/builder/context.dart';
 export 'src/expressions/boolean.dart';
 export 'src/expressions/expression.dart';
 export 'src/schema.dart';
+export 'src/statements/statement.dart' show SqlStatement;
 export 'src/statements/delete.dart';
 export 'src/statements/insert.dart';
 export 'src/statements/select.dart' show SelectStatement, star;
