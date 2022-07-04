b1c87add80f38ec2fd0f3eca1f4db2173af59cc6
Alexander Wilde <alexander.wilde87@gmail.com>
2022-07-04 21:24:36 +0100

    Tests for julianday, unixepoch, strftime

--- drift/test/database/expressions/expressions_integration_test.dart
+++ drift/test/database/expressions/expressions_integration_test.dart
@@ -52,6 +52,10 @@ void main() {
     expect(eval(expr.date()), completion('2020-09-03'));
     expect(eval(expr.time()), completion('23:55:00'));
     expect(eval(expr.datetime()), completion('2020-09-03 23:55:00'));
+    expect(eval(expr.julianday()), completion('2459096.496527778'));
+    expect(eval(expr.unixepoch()), completion('1599177300'));
+    expect(eval(expr.strftime('%Y-%m-%d %H:%M:%S')),
+        completion('2020-09-03 23:55:00'));
 
     // Really hacky...
     final localDate = local.toIso8601String().substring(0, 10);
