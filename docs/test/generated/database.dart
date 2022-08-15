74ab9d4f806467539e9fc3045ef4197ff4a26b3c
Simon Binder <oss@simonbinder.eu>
2022-08-15 19:55:06 +0200

    Move snippet test to docs package

---
+++ docs/test/generated/database.dart
@@ -0,0 +1,21 @@
+import 'package:drift/drift.dart';
+
+part 'database.g.dart';
+
+class Users extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text()();
+  DateTimeColumn get createdAt =>
+      dateTime().nullable().withDefault(currentDateAndTime)();
+}
+
+@DriftDatabase(tables: [Users])
+class Database extends _$Database {
+  Database.connect(DatabaseConnection c) : super.connect(c);
+
+  @override
+  int get schemaVersion => 1;
+
+  @override
+  DriftDatabaseOptions options = DriftDatabaseOptions();
+}
