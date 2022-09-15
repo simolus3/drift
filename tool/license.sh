1fbd42b475462c8a390110345b5b0cbd1c3d2418
Simon Binder <oss@simonbinder.eu>
2022-09-15 22:45:15 +0200

    Use melos

--- tool/license.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f moor/LICENSE
-rm -f moor_flutter/LICENSE
-rm -f moor_generator/LICENSE
-
-cp LICENSE moor/LICENSE
-cp LICENSE moor_flutter/LICENSE
-cp LICENSE moor_generator/LICENSE
