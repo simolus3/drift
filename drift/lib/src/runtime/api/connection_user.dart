0de8e9fed312a6becea5f9c21c90f7d2ed03b640
Simon Binder <oss@simonbinder.eu>
2022-11-07 16:18:17 +0100

    Fix snippet test

--- drift/lib/src/runtime/api/connection_user.dart
+++ drift/lib/src/runtime/api/connection_user.dart
@@ -19,7 +19,7 @@ abstract class DatabaseConnectionUser {
 
   /// A [SqlTypes] mapping configuration to use when mapping values between Dart
   /// and SQL.
-  late final SqlTypes typeMapping = options.createTypeMapping(executor.dialect);
+  SqlTypes get typeMapping => options.createTypeMapping(executor.dialect);
 
   /// The database class that this user is attached to.
   @visibleForOverriding
