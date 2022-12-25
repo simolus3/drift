47579a2b91e60210c649d239fd7d651595756705
Simon Binder <oss@simonbinder.eu>
2022-12-26 00:06:02 +0100

    Ignore temporary implementation import

--- drift_dev/lib/src/analysis/resolver/dart/helper.dart
+++ drift_dev/lib/src/analysis/resolver/dart/helper.dart
@@ -1,6 +1,7 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
+// ignore: implementation_imports
 import 'package:analyzer/src/dart/element/type.dart'
     show
         RecordTypeImpl,
