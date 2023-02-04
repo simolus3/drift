9ba706fb496692c34dc0180cd93ab222824fad67
David Martos <davidmartos96@gmail.com>
2023-02-04 16:44:38 +0100

    simplified test

--- extras/lock_bug/test/widget_test.dart
+++ extras/lock_bug/test/widget_test.dart
@@ -1,20 +1,6 @@
 import 'package:drift/native.dart';
 import 'package:drift_lock_bug/database.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-final databaseProvider = Provider<Database>((ref) {
-  throw UnimplementedError();
-});
-
-final myFutureProvider = FutureProvider<int>((ref) async {
-  final db = ref.watch(databaseProvider);
-
-  await db.customSelect("SELECT 1").get();
-
-  return 1;
-});
 
 void main() {
   testWidgets("deadlock test", (tester) async {
@@ -22,32 +8,12 @@ void main() {
       NativeDatabase.memory(logStatements: true),
     );
 
-    final container = ProviderContainer(
-      overrides: [
-        databaseProvider.overrideWithValue(db),
-      ],
-    );
-
     addTearDown(() async {
       print("closing db");
       await db.close();
       print("db closed");
-      container.dispose();
-      print("finish teardown");
     });
 
-    final widget = UncontrolledProviderScope(
-      container: container,
-      child: Consumer(
-        builder: (BuildContext context, WidgetRef ref, Widget? child) {
-          final dbRes = ref.watch(myFutureProvider);
-          print("db result $dbRes");
-
-          return const SizedBox();
-        },
-      ),
-    );
-
-    await tester.pumpWidget(widget);
+    await db.customSelect("SELECT 1").get();
   });
 }
