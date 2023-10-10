6bf32b5bf8c761ddf586ee1cb159a5828ba93a01
Simon Binder <oss@simonbinder.eu>
2023-10-10 21:58:05 +0200

    Update assets from `flutter pub get`

--- examples/encryption/windows/flutter/generated_plugin_registrant.cc
+++ examples/encryption/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <sqlcipher_flutter_libs/sqlite3_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  Sqlite3FlutterLibsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
 }
--- examples/encryption/windows/flutter/generated_plugins.cmake
+++ examples/encryption/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  sqlcipher_flutter_libs
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
