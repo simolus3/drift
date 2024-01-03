f776471abe8257b919c26eb44a76a4c773b875e4
Tree NG <tt88dev@gmail.com>
2024-01-03 14:21:49 +0800

    [bugfix/aggregate_test] - Bugfix feature - Add More Aggregate Test for String

--- drift/test/database/expressions/aggregate_test.dart
+++ drift/test/database/expressions/aggregate_test.dart
@@ -17,14 +17,20 @@ void main() {
         countAll(filter: foo.isBiggerOrEqualValue(3)),
         generates('COUNT(*) FILTER (WHERE foo >= ?)', [3]),
       );
+      expect(
+        countAll(filter: s1.equals('STRING_VALUE')),
+        generates('COUNT(*) FILTER (WHERE s1 = ?)', ['STRING_VALUE']),
+      );
     });
 
     test('single', () {
       expect(foo.count(), generates('COUNT(foo)'));
+      expect(s1.count(), generates('COUNT(s1)'));
     });
 
     test('single - distinct', () {
       expect(foo.count(distinct: true), generates('COUNT(DISTINCT foo)'));
+      expect(s1.count(distinct: true), generates('COUNT(DISTINCT s1)'));
     });
 
     test('single - filter', () {
@@ -32,12 +38,20 @@ void main() {
         foo.count(filter: foo.isBiggerOrEqualValue(3)),
         generates('COUNT(foo) FILTER (WHERE foo >= ?)', [3]),
       );
+      expect(
+        s1.count(filter: s1.equals('STRING_VALUE')),
+        generates('COUNT(s1) FILTER (WHERE s1 = ?)', ['STRING_VALUE']),
+      );
     });
 
     test('single - distinct and filter', () {
       expect(
         foo.count(distinct: true, filter: foo.isBiggerOrEqualValue(3)),
         generates('COUNT(DISTINCT foo) FILTER (WHERE foo >= ?)', [3]),
+      );
+      expect(
+        s1.count(distinct: true, filter: s1.equals('STRING_VALUE')),
+        generates('COUNT(DISTINCT s1) FILTER (WHERE s1 = ?)', ['STRING_VALUE']),
       );
     });
   });
@@ -69,22 +83,33 @@ void main() {
   group('group_concat', () {
     test('with the default separator', () {
       expect(foo.groupConcat(), generates('GROUP_CONCAT(foo)'));
+      expect(s1.groupConcat(), generates('GROUP_CONCAT(s1)'));
+
       expect(foo.groupConcat(separator: ','), generates('GROUP_CONCAT(foo)'));
+      expect(s1.groupConcat(separator: ','), generates('GROUP_CONCAT(s1)'));
     });
 
     test('with a custom separator', () {
       expect(foo.groupConcat(separator: ' and '),
           generates('GROUP_CONCAT(foo, ?)', [' and ']));
+      expect(s1.groupConcat(separator: ' and '),
+          generates('GROUP_CONCAT(s1, ?)', [' and ']));
     });
 
     test('with a filter', () {
       expect(foo.groupConcat(filter: foo.isSmallerThan(const Variable(3))),
           generates('GROUP_CONCAT(foo) FILTER (WHERE foo < ?)', [3]));
+      expect(
+          s1.groupConcat(filter: s1.isSmallerThan(Variable('STRING_VALUE'))),
+          generates(
+              'GROUP_CONCAT(s1) FILTER (WHERE s1 < ?)', ['STRING_VALUE']));
     });
 
     test('with distinct', () {
       expect(foo.groupConcat(distinct: true),
           generates('GROUP_CONCAT(DISTINCT foo)', isEmpty));
+      expect(s1.groupConcat(distinct: true),
+          generates('GROUP_CONCAT(DISTINCT s1)', isEmpty));
 
       expect(
         foo.groupConcat(
@@ -96,10 +121,22 @@ void main() {
           [3],
         ),
       );
+      expect(
+        s1.groupConcat(
+          distinct: true,
+          filter: s1.isSmallerThan(Variable('STRING_VALUE')),
+        ),
+        generates(
+          'GROUP_CONCAT(DISTINCT s1) FILTER (WHERE s1 < ?)',
+          ['STRING_VALUE'],
+        ),
+      );
     });
 
     test('does not allow distinct with a custom separator', () {
       expect(() => foo.groupConcat(distinct: true, separator: ' and '),
+          throwsArgumentError);
+      expect(() => s1.groupConcat(distinct: true, separator: ' and '),
           throwsArgumentError);
 
       expect(
@@ -107,6 +144,14 @@ void main() {
           distinct: true,
           separator: ' and ',
           filter: foo.isSmallerThan(const Variable(3)),
+        ),
+        throwsArgumentError,
+      );
+      expect(
+        () => s1.groupConcat(
+          distinct: true,
+          separator: ' and ',
+          filter: s1.isSmallerThan(Variable('STRING_VALUE')),
         ),
         throwsArgumentError,
       );
