a1af6f6114960caaee6a9d7699e27f92cc8c93dc
Simon Binder <oss@simonbinder.eu>
2024-04-27 22:46:24 +0200

    Fix opening library in encryption example

--- examples/encryption/lib/database.dart
+++ examples/encryption/lib/database.dart
@@ -1,9 +1,12 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
+import 'package:sqlcipher_flutter_libs/sqlcipher_flutter_libs.dart';
+import 'package:sqlite3/open.dart';
 
 part 'database.g.dart';
 
@@ -39,6 +42,14 @@ QueryExecutor _openDatabase() {
 
     return NativeDatabase.createInBackground(
       File(p.join(path.path, 'app.db.enc')),
+      isolateSetup: () async {
+        open
+          ..overrideFor(OperatingSystem.android, openCipherOnAndroid)
+          ..overrideFor(OperatingSystem.linux,
+              () => DynamicLibrary.open('libsqlcipher.so'))
+          ..overrideFor(OperatingSystem.windows,
+              () => DynamicLibrary.open('sqlcipher.dll'));
+      },
       setup: (db) {
         // Check that we're actually running with SQLCipher by quering the
         // cipher_version pragma.
