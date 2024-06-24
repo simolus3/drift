d70baececf93e0f50f57631e03005f181a6d0f87
Simon Binder <oss@simonbinder.eu>
2024-06-24 23:19:55 +0200

    Add example

--- drift_flutter/example/main.dart
+++ drift_flutter/example/main.dart
@@ -1,1 +1,23 @@
+import 'package:drift/drift.dart';
+import 'package:drift_flutter/drift_flutter.dart';
 
+part 'main.g.dart';
+
+void main() async {
+  final database = ExampleDatabase();
+  await database.exampleTable.all().get();
+}
+
+class ExampleTable extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get description => text()();
+}
+
+@DriftDatabase(tables: [ExampleTable])
+final class ExampleDatabase extends _$ExampleDatabase {
+  ExampleDatabase([QueryExecutor? implementation])
+      : super(implementation ?? driftDatabase(name: 'db'));
+
+  @override
+  int get schemaVersion => 1;
+}
