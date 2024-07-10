e2a09003a1cfae3d02fcdd8ec898c3071ff32c8a
Moshe Dicker <dickermoshe@gmail.com>
2024-07-10 09:36:19 -0400

    chore: remove unused code

--- drift_dev/lib/src/writer/manager/manager_templates.dart
+++ drift_dev/lib/src/writer/manager/manager_templates.dart
@@ -560,9 +560,3 @@ class _ManagerCodeTemplates {
       }""";
   }
 }
-
-String _generateRandomString(int len) {
-  var r = Random();
-  const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
-  return List.generate(len, (index) => chars[r.nextInt(chars.length)]).join();
-}
