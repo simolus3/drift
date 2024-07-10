d2e056e18d8ab22281aeab71cc2e18afed96f4a3
Moshe Dicker <dickermoshe@gmail.com>
2024-07-10 09:41:36 -0400

    chore: remove unused import

--- drift_dev/lib/src/writer/manager/database_manager_writer.dart
+++ drift_dev/lib/src/writer/manager/database_manager_writer.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:collection/collection.dart';
 import 'package:drift_dev/src/analysis/results/results.dart';
 import 'package:drift_dev/src/writer/tables/update_companion_writer.dart';
