9e9b7482e5bb092c8b860e2310f5ee7a6e76787a
Simon Binder <oss@simonbinder.eu>
2024-07-18 14:41:45 +0200

    Demangle stack traces in example app

--- examples/app/lib/main.dart
+++ examples/app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stack_trace/stack_trace.dart';
 import 'package:go_router/go_router.dart';
 
 import 'screens/home.dart';
 import 'screens/search.dart';
 
 void main() {
+  FlutterError.demangleStackTrace = (StackTrace stack) {
+    if (stack is Trace) return stack.vmTrace;
+    if (stack is Chain) return stack.toTrace().vmTrace;
+    return stack;
+  };
+
   runApp(MyApp());
 }
 
