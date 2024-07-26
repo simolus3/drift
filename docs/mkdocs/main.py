522246f52c214958f30f5c04da8aa0eb02d38461
Moshe Dicker <dickermoshe@gmail.com>
2024-07-26 16:50:19 -0400

    docs: default highlight

--- docs/mkdocs/main.py
+++ docs/mkdocs/main.py
@@ -1,10 +1,32 @@
 ### This function delares macros which can be used in the documentation files.
 import json
 from pathlib import Path
-import random
-import string
+from io import StringIO
+from html.parser import HTMLParser
+from textwrap import indent as indent_text, dedent
 
 current_dir = Path(__file__).parent
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 
 def define_env(env):
@@ -15,7 +37,7 @@ def define_env(env):
     env.variables["versions"] = versions
 
     @env.macro
-    def load_snippet(snippet_name: str, *args: list[str], indent: int = 0):
+    def load_snippet(snippet_name: str, *args: str, indent: int = 0):
         """
         This macro allows to load a snippets from source files and display them in the documentation.
 
@@ -31,27 +53,24 @@ def define_env(env):
         data = {}
         for file in files:
             data.update(json.loads(file.read_text()))
-        return code_template(data[snippet_name], indent)
+
+        # Remove the HTML tags from the content and remove extra indentations.
+        content = dedent(strip_tags(data[snippet_name]))
+
+        # If this is not a dart file, we will return the content in a regular code block.
+        result: str
+        is_dart = any(".dart.excerpt.json" in str(file) for file in files)
+        is_drift = any(".drift.excerpt.json" in str(file) for file in files)
+        if is_dart:
+            result = markdown_codeblock(content, "dart")
+        if is_drift:
+            result = markdown_codeblock(content, "sql")
+        else:
+            result = markdown_codeblock(content)
+
+        # Add the indent to the snippet, besides for the first line which is already indented.
+        return indent_text(result, indent * " ").lstrip()
 
 
-def code_template(content: str, indent: int = 0):
-    """
-    Create the html for this code block.
-    """
-    random_id = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
-    # Split the content by new line
-    content = content.splitlines()
-
-    # Add indent to each line, besides the first line
-    lines = content[1:]
-    content = [content[0]] + [f"{' '*indent}{line}" for line in lines]
-
-    # Replace blank lines with <br> tag
-    content = [
-        line if len(line.strip()) > 0 else f"{' '*indent}<br>" for line in content
-    ]
-
-    content = "\n".join(content)
-
-    result = f"""<pre id="{random_id}"><code>{content}</code></pre>"""
-    return result
+def markdown_codeblock(content: str, lang: str = "") -> str:
+    return f"```{lang}\n{content}\n```"
