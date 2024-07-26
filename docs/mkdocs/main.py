1672d51d3d7e635853b17545f72f5214abb415f4
Moshe Dicker <dickermoshe@gmail.com>
2024-07-26 13:50:09 -0400

    docs: add more docdocs

--- docs/mkdocs/main.py
+++ docs/mkdocs/main.py
@@ -3,27 +3,42 @@ import json
 from pathlib import Path
 import random
 import string
+
 current_dir = Path(__file__).parent
 
-version_file = Path(__file__).parent.parent / "lib" / "versions.json"
-def define_env(env):
-    # Read the versions.json file
-    versions:dict[str,str] = json.loads(version_file.read_text())
-    env.variables['versions'] = versions
 
+def define_env(env):
+    # Read the versions.json file in `/lib`
+    # This allows `{{ versions.drift }}` in the markdown files
+    version_file = Path(__file__).parent.parent / "lib" / "versions.json"
+    versions: dict[str, str] = json.loads(version_file.read_text())
+    env.variables["versions"] = versions
 
     @env.macro
-    def load_snippet(snippet_name:str, *args:list[str], indent:int=0):
+    def load_snippet(snippet_name: str, *args: list[str], indent: int = 0):
+        """
+        This macro allows to load a snippets from source files and display them in the documentation.
+
+        The snippet_name referes to whatever the snippet is named in the source file (e.g. `setup` from  `// #docregion setup``)
+
+        The args are a list of files to load the snippet from. (e.g 'lib/snippets/setup/database.dart.excerpt.json')
+
+        The indent is the number of spaces to indent the snippet by. (default is 0)
+        This is used when placing a snippet inside tabs.
+        See the `docs` for examples where indentation is used.
+        """
         files = [current_dir.parent / i for i in args]
         data = {}
         for file in files:
             data.update(json.loads(file.read_text()))
         return code_template(data[snippet_name], indent)
-            
 
 
-def code_template(content:str,indent:int=0):
-    random_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+def code_template(content: str, indent: int = 0):
+    """
+    Create the html for this code block.
+    """
+    random_id = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
     # Split the content by new line
     content = content.splitlines()
 
@@ -32,10 +47,11 @@ def code_template(content:str,indent:int=0):
     content = [content[0]] + [f"{' '*indent}{line}" for line in lines]
 
     # Replace blank lines with <br> tag
-    content = [line if len(line.strip()) > 0 else f"{' '*indent}<br>" for line in content ] 
-
+    content = [
+        line if len(line.strip()) > 0 else f"{' '*indent}<br>" for line in content
+    ]
 
     content = "\n".join(content)
 
-    result =  f"""<pre id="{random_id}"><code>{content}</code></pre>"""
+    result = f"""<pre id="{random_id}"><code>{content}</code></pre>"""
     return result
