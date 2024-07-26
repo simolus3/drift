471bd382a56160613ba6fc82a7a5811bd13c3e0a
Moshe Dicker <dickermoshe@gmail.com>
2024-07-26 16:45:19 -0400

    Revert "add shiki"
    This reverts commit ec3a8665e89f273026c2f627cc008ea8c4f218f2.

--- docs/mkdocs/main.py
+++ docs/mkdocs/main.py
@@ -1,33 +1,10 @@
 ### This function delares macros which can be used in the documentation files.
 import json
 from pathlib import Path
-from io import StringIO
-from html.parser import HTMLParser
-from textwrap import indent as indent_text
-import subprocess
+import random
+import string
 
 current_dir = Path(__file__).parent
-
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs = True
-        self.text = StringIO()
-
-    def handle_data(self, d):
-        self.text.write(d)
-
-    def get_data(self):
-        return self.text.getvalue()
-
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
 
 
 def define_env(env):
@@ -38,7 +15,7 @@ def define_env(env):
     env.variables["versions"] = versions
 
     @env.macro
-    def load_snippet(snippet_name: str, *args: str, indent: int = 0):
+    def load_snippet(snippet_name: str, *args: list[str], indent: int = 0):
         """
         This macro allows to load a snippets from source files and display them in the documentation.
 
@@ -54,57 +31,27 @@ def define_env(env):
         data = {}
         for file in files:
             data.update(json.loads(file.read_text()))
-
-        content = strip_tags(data[snippet_name])
-
-        # If this is not a dart file, we will return the content in a regular code block.
-        result: str
-        is_dart = any(".dart.excerpt.json" in str(file) for file in files)
-        is_drift = any(".drift.excerpt.json" in str(file) for file in files)
-        if not is_dart:
-            if is_drift:
-                result = markdown_codeblock(content, "sql")
-            else:
-                result = markdown_codeblock(content)
-        else:
-            result = html_codeblock(content)
-
-        # Add the indent to the snippet, besides for the first line which is already indented.
-        return indent_text(result, indent * " ").lstrip()
-
-        # # Remove the minimum indentation from each line
-        # html = "\n".join([line[min_indent:] for line in html.split("\n")])
-
-        # # Build the snippet
-        # snippet = f"""```{"dart" if is_dart else ""}""" + f"\n{html}\n" + """```"""
-
-        # # Indent the snippet by the specified amount besides for the 1st line which is already indented.
-        # return "\n".join(
-        #     [
-        #         " " * indent + line if i > 0 else line
-        #         for i, line in enumerate(snippet.split("\n"))
-        #     ]
-        # )
-
-        # return code_template(data[snippet_name], indent)
+        return code_template(data[snippet_name], indent)
 
 
-def html_codeblock(content: str) -> str:
+def code_template(content: str, indent: int = 0):
     """
     Create the html for this code block.
     """
-    result = subprocess.run(
-        ["npm", "run", "highlight", "--silent", "--", "--input", content],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        cwd=current_dir,
-    )
-    if result.returncode != 0:
-        raise Exception(
-            f"Failed to highlight code block: {result.stdout} {result.stderr}"
-        )
-    return result.stdout.decode("utf-8")
+    random_id = "".join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    # Split the content by new line
+    content = content.splitlines()
 
+    # Add indent to each line, besides the first line
+    lines = content[1:]
+    content = [content[0]] + [f"{' '*indent}{line}" for line in lines]
 
-def markdown_codeblock(content: str, lang: str = "") -> str:
-    return f"```{lang}\n{content}\n```"
+    # Replace blank lines with <br> tag
+    content = [
+        line if len(line.strip()) > 0 else f"{' '*indent}<br>" for line in content
+    ]
+
+    content = "\n".join(content)
+
+    result = f"""<pre id="{random_id}"><code>{content}</code></pre>"""
+    return result
