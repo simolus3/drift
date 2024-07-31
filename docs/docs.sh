eaec061d843a538ab00e22f43c880166f619f20a
Moshe Dicker <dickermoshe@gmail.com>
2024-07-31 03:17:42 -0400

    docs: don't use cdn

--- docs/docs.sh
+++ docs/docs.sh
@@ -54,7 +54,7 @@ if [ $arg1 == "build" ]; then
         echo "Failed to build the example project"
         exit 1
     fi
-    flutter build web --base-href "/examples/app/"
+    flutter build web --base-href "/examples/app/" --no-web-resources-cdn
     if [ $? -ne 0 ]; then
         echo "Failed to build the example project"
         exit 1
