2bd9632582276e06d20ef12d568efb5e8b2e8656
Simon Binder <oss@simonbinder.eu>
2024-08-18 20:38:45 +0200

    Update has local sqlite3 test to check version

--- drift/test/test_utils/has_local_sqlite3_test.dart
+++ drift/test/test_utils/has_local_sqlite3_test.dart
@@ -5,15 +5,34 @@ import 'dart:io';
 
 import 'package:test/test.dart';
 
+import '../../tool/download_sqlite3.dart' as download;
 import 'database_vm.dart';
 
 void main() {
+  preferLocalSqlite3();
+
   final path = expectedLocalSqlite3Path;
   final printWarning = path != null && !File(path).existsSync();
 
   test(
     'check for local sqlite3 library',
-    () {},
+    () {
+      var actual = sqlite3Version.versionNumber;
+      // https://www.sqlite.org/c3ref/c_source_id.html
+      var major = actual ~/ 1000000;
+      var minor = (actual % 1000000) ~/ 1000;
+      var patch = actual % 1000;
+
+      var downloadCode = 100 * (patch + 100 * (minor + 100 * major));
+      expect(
+        downloadCode.toString(),
+        download.latest.version,
+        reason: "Your local sqlite3 build used to test drift doesn't match "
+            'the expected version, possibly leading to failing tests. '
+            'You have $downloadCode, drift expects ${download.latest}. '
+            'Please run `dart run too/download_sqlite3.dart` to fix this.',
+      );
+    },
     skip: printWarning
         ? 'Local sqlite3 library for drift tests does not exist, falling back '
             'to the one from the OS. Please run `dart run tool/download_sqlite3.dart`'
