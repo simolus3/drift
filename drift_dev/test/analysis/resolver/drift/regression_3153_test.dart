93fe426d26499adf2ca3e95829b0e447656f5ee6
Adam Langley <github.com@irisdesign.co.nz>
2024-08-19 12:56:51 +1200

    Create regression_3153_test.dart

---
+++ drift_dev/test/analysis/resolver/drift/regression_3153_test.dart
@@ -0,0 +1,40 @@
+import 'package:test/test.dart';
+
+import '../../test_utils.dart';
+
+void main() {
+  test('does not include private superclasses reached via .drift', () async {
+    final backend = await TestBackend.inTest({
+      'a|lib/database.dart': '''
+import 'package:drift/drift.dart';
+import 'tables.dart';
+
+@DriftDatabase(include: {'test.drift'}, tables: [PartOfDatabase])
+class Database {}
+''',
+      'a|lib/tables.dart': '''
+import 'package:drift/drift.dart';
+
+class PartOfDatabase extends _NotPartOfDatabase {
+
+}
+
+class _NotPartOfDatabase extends Table {
+  IntColumn get id => integer()();
+}
+
+''',
+      'a|lib/test.drift': '''
+import 'tables.dart';
+''',
+    });
+
+    final state = await backend.analyze('package:a/database.dart');
+    backend.expectNoErrors();
+
+    final database = state.fileAnalysis!.resolvedDatabases.values.single;
+    
+    expect(database.availableElements, hasLength(1));
+    expect(database.availableElements.first.declaration.name, equals('PartOfDatabase'));
+  });
+}
