df01b18d5dc832fd8d264262e9d8f4afed206134
Simon Binder <oss@simonbinder.eu>
2024-08-21 18:21:34 +0200

    Add test for parenthesis generation around bools
    https://github.com/simolus3/drift/issues/3165

--- drift/test/database/expressions/bools_test.dart
+++ drift/test/database/expressions/bools_test.dart
@@ -6,6 +6,8 @@ import '../../test_utils/test_utils.dart';
 void main() {
   const a = CustomExpression<bool>('a', precedence: Precedence.primary);
   const b = CustomExpression<bool>('b', precedence: Precedence.primary);
+  const c = CustomExpression<bool>('c', precedence: Precedence.primary);
+  const d = CustomExpression<bool>('d', precedence: Precedence.primary);
 
   test('boolean expressions via operators', () {
     expect(a | b, generates('a OR b'));
@@ -14,5 +16,22 @@ void main() {
 
     expectEquals(a & b, a & b);
     expectNotEquals(a | b, b | a);
+  });
+
+  test('respects precedence', () {
+    expect(a | b & c, generates('a OR b AND c'));
+    expect(a | (b & c), generates('a OR b AND c'));
+    expect((a | b) & c, generates('(a OR b) AND c'));
+
+    expect(
+      Expression.and([
+        a,
+        Expression.or([
+          b,
+          Expression.and([c, d]),
+        ]),
+      ]),
+      generates('a AND (b OR c AND d)'),
+    );
   });
 }
