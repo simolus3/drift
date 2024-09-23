27e942938b6ed9e0930605f66da12dddf245daea
Simon Binder <oss@simonbinder.eu>
2024-09-24 00:46:47 +0200

    Fix conditional exports for web tests

--- drift/test/test_utils/test_utils.dart
+++ drift/test/test_utils/test_utils.dart
@@ -5,8 +5,8 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 export 'database_stub.dart'
-    if (dart.library.ffi) 'database_vm.dart'
-    if (dart.library.js) 'database_web.dart';
+    if (dart.library.io) 'database_vm.dart'
+    if (dart.library.js_interop) 'database_web.dart';
 export 'matchers.dart';
 export 'mocks.dart';
 
