cc7a1979b79f9cabd31db312c2f9c92f42ff0daf
Moshe Dicker <dickermoshe@gmail.com>
2024-10-13 23:08:55 -0400

    attr dart code

--- drift_dev/lib/src/lints/unawaited_futures_in_transaction.dart
+++ drift_dev/lib/src/lints/unawaited_futures_in_transaction.dart
@@ -39,6 +39,7 @@ class UnawaitedFuturesInTransaction extends DartLintRule {
 // Copyright (c) 2016, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+// Source: https://github.com/dart-lang/sdk/blob/main/pkg/linter/lib/src/rules/unawaited_futures.dart
 class _Visitor extends SimpleAstVisitor<void> {
   final LintRule rule;
   final ErrorReporter reporter;
