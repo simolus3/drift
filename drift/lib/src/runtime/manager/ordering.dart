6d8cce6a72ace522b352df4c52a6bbe8497314de
Moshe Dicker <75931499+dickermoshe@users.noreply.github.com>
2024-10-20 23:23:55 -0400

    Update ordering.dart

--- drift/lib/src/runtime/manager/ordering.dart
+++ drift/lib/src/runtime/manager/ordering.dart
@@ -21,13 +21,13 @@ class ColumnOrderings<T extends Object> {
 
   /// Sort this column in ascending order
   ///
-  /// 10 -> 1 | Z -> A | Dec 31 -> Jan 1
+  /// 1 -> 10 | A -> Z | Jan 1 -> Dec 31
   ComposableOrdering asc() =>
       $composableOrdering({OrderingBuilder(OrderingMode.asc, column)});
 
   /// Sort this column in descending order
   ///
-  ///  1 -> 10 | A -> Z | Jan 1 -> Dec 31
+  /// 10 -> 1 | Z -> A | Dec 31 -> Jan 1
   ComposableOrdering desc() =>
       $composableOrdering({OrderingBuilder(OrderingMode.desc, column)});
 }
