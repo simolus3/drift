3c0ee561780c5daf0b3275c9c7baa6a696f28132
Lucas SAUDON <lucas.saudon.ext@homeserve.fr>
2026-06-02 14:04:16 +0200

    Fix sqlite 3.52

--- sqlparser/lib/src/engine/options.dart
+++ sqlparser/lib/src/engine/options.dart
@@ -119,7 +119,7 @@ class SqliteVersion implements Comparable<SqliteVersion> {
 
   /// Version `3.52.0` added the `json_array_insert` and `jsonb_array_insert`
   /// functions and enhanced `ALTER TABLE`.
-  static const SqliteVersion v3_53 = SqliteVersion.v3(53);
+  static const SqliteVersion v3_52 = SqliteVersion.v3(52);
 
   /// Version `3.51.0` added the `jsonb_each` and `jsonb_tree` functions.
   static const SqliteVersion v3_51 = SqliteVersion.v3(51);
